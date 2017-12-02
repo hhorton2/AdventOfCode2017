7570addd8e872ce14a8db6b70aa8955efb775e4d
Hunter Horton <hunterhorton89@gmail.com>
2017-12-01 23:19:56 -0600

    solving day 2

--- Day02/day2.go
+++ Day02/day2.go
@@ -3,13 +3,55 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	dat, err := ioutil.ReadFile("Day02/input.txt")
 	check(err)
 	datString := string(dat)
-	fmt.Println(datString)
+	rows := strings.Split(datString, "\n")
+	checkSum := getChecksum(rows)
+	divisibleCheckSum := getDivisibleChecksum(rows)
+	fmt.Println(checkSum)
+	fmt.Println(divisibleCheckSum)
+}
+func getChecksum(rows []string) int {
+	checksum := 0
+	for _, row := range rows {
+		min := math.MaxInt32
+		max := math.MinInt32
+		for _, column := range strings.Split(row, "	") {
+			currentNum, _ := strconv.Atoi(column)
+			if currentNum < min {
+				min = currentNum
+			} else if currentNum > max {
+				max = currentNum
+			}
+		}
+		checksum += max - min
+	}
+	return checksum
+}
+
+func getDivisibleChecksum(rows []string) int {
+	checksum := 0
+	for _, row := range rows {
+		for i, column := range strings.Split(row, "	") {
+			currentNum, _ := strconv.Atoi(column)
+			for j, compareColumn := range strings.Split(row, "	") {
+				if i != j {
+					compareNum, _ := strconv.Atoi(compareColumn)
+					if currentNum%compareNum == 0 {
+						checksum += currentNum / compareNum
+					}
+				}
+			}
+		}
+	}
+	return checksum
 }
 
 func check(e error) {
