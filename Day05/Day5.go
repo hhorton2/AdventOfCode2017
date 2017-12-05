ac0fad431f5908350145fcf992a00dafd44f82d6
Hunter Horton <hhorton@atlasrfid.com>
2017-12-05 09:23:55 -0600

    Adding concurrency to Day 5

--- Day05/Day5.go
+++ Day05/Day5.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"fmt"
 	"time"
+	"sync"
 )
 
 func main() {
@@ -13,14 +14,22 @@ func main() {
 	dat, err := ioutil.ReadFile("Day05/input.txt")
 	check(err)
 	datString := string(dat)
-	program := convertStringToIntArray(datString)
-	solvePartOne(program)
-	program = convertStringToIntArray(datString)
-	solvePartTwo(program)
+	var wg sync.WaitGroup
+	var program []int
+	var program2 []int
+	wg.Add(2)
+	go convertStringToIntArray(datString, &program, &wg)
+	go convertStringToIntArray(datString, &program2, &wg)
+	wg.Wait()
+	wg.Add(2)
+	go solvePartOne(program, &wg)
+	go solvePartTwo(program2, &wg)
+	wg.Wait()
 	fmt.Printf("%v", time.Since(start))
 }
 
-func solvePartTwo(program []int) {
+func solvePartTwo(program []int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	position := 0
 	count := 0
 	for position < len(program) {
@@ -36,7 +45,8 @@ func solvePartTwo(program []int) {
 	fmt.Println(count)
 }
 
-func solvePartOne(program []int) {
+func solvePartOne(program []int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	position := 0
 	count := 0
 	for position < len(program) {
@@ -48,13 +58,12 @@ func solvePartOne(program []int) {
 	fmt.Println(count)
 }
 
-func convertStringToIntArray(input string) []int {
-	var program []int
+func convertStringToIntArray(input string, program *[]int, wg *sync.WaitGroup){
+	defer wg.Done()
 	for _, letter := range strings.Split(input, "\n") {
 		number, _ := strconv.Atoi(letter)
-		program = append(program, number)
+		*program = append(*program, number)
 	}
-	return program
 }
 
 func check(e error) {
